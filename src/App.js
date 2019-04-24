91469ce9dc051f44d9cd19d04c1332fa2099cc59
LuciOroles <lucian.stoica@gfk.com>
2019-04-24 17:07:55 +0300

    testing tmdb api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: []
+    };
+  }
+
+  componentDidMount() {
+    let tmdbRequest = new Request(
+      "https://api.themoviedb.org/3/search/movie?include_adult=false&page=1&query=game%20of%20thrones&language=en-US&api_key=d1e5532be9d5c4de6ad35098d60b22a2",
+      { method: "GET" }
+    );
+    // fetch(tmdbRequest)
+    //   .then(resp => {
+    //     return resp.json();
+    //   })
+    //   .catch(console.error)
+    //   .then(data => {
+    //     debugger;
+    //     this.setState({
+    //       movies: data.results
+    //     });
+    //   })
+    //   .catch(console.error);
+  }
+
+  render() {
+    var { movies } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">React-Redux-Movie LeoVegas</header>
+        {movies.length === 0 ? (
+          <p>No movies yet</p>
+        ) : (
+          movies
+            .map(movie => {
+              debugger;
+              return <div key={movie.id}>{movie.title}</div>;
+            })
+            .concat("")
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
